1343ecb3279995ae34f57e9d49345f195829c97e
Gustavo <tavinhogustavog@gmail.com>
2021-03-25 23:42:37 -0300

    inclusão de inimigos e colisões

--- space_rocks/game.py
+++ space_rocks/game.py
@@ -1,24 +1,36 @@
 import pygame
-from utils import load_sprite
-from models import GameObject
+from utils import load_sprite, get_random_position
+from models import Spaceship, Asteroid
 
 class SpaceRocks:
+    MIN_ASTEROID_DISTANCE = 250
+
     def __init__(self):
         self._init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
-        self.backgorund = load_sprite("space", False)
-        self.spaceship = GameObject(
-            (400, 300), load_sprite("spaceship"), (0, 0)
-        )
-        self.asteroid = GameObject(
-            (400, 300), load_sprite("asteroid"), (1, 0)
-        )
-    
+        self.background = load_sprite("space", False)
+        self.clock = pygame.time.Clock()
+
+        self.asteroids = []
+        self.spaceship = Spaceship((400, 300))
+
+        for _ in range(6):
+            while True:
+                position = get_random_position(self.screen)
+                if (
+                    position.distance_to(self.spaceship.position)
+                    > self.MIN_ASTEROID_DISTANCE
+                ):
+                    break
+
+            self.asteroids.append(Asteroid(position))
+
     def main_loop(self):
         while True:
             self._handle_input()
             self._process_game_logic()
             self._draw()
+
 
     def _init_pygame(self):
         pygame.init()
@@ -31,14 +43,40 @@ class SpaceRocks:
             ):
                 quit()
 
+        is_key_pressed = pygame.key.get_pressed()
+
+        if is_key_pressed[pygame.K_RIGHT]:
+            self.spaceship.rotate(clockwise=True)
+        if is_key_pressed[pygame.K_LEFT]:
+            self.spaceship.rotate(clockwise=False)
+        if is_key_pressed[pygame.K_UP]:
+            self.spaceship.accelerate()
+            
     def _process_game_logic(self):
-        self.spaceship.move()
-        self.asteroid.move()
+        for game_object in self._get_game_objects():
+            game_object.move(self.screen)
+
+        if self.spaceship:
+            for asteroid in self.asteroids:
+                if asteroid.collides_with(self.spaceship):
+                    self.spaceship = None
+                    break
 
     def _draw(self):
-        self.screen.blit(self.backgorund, (0, 0))
-        self.spaceship.draw(self.screen)
-        self.asteroid.draw(self.screen)
+        self.screen.blit(self.background, (0, 0))
+
+        for game_object in self._get_game_objects():
+            game_object.draw(self.screen)
+
         pygame.display.flip()
+        self.clock.tick(60)
+
+    def _get_game_objects(self):
+        game_objects = [*self.asteroids]
+
+        if self.spaceship:
+            game_objects.append(self.spaceship)
+
+        return game_objects
         
 
--- space_rocks/models.py
+++ space_rocks/models.py
@@ -1,4 +1,9 @@
 from pygame.math import Vector2
+from pygame.transform import rotozoom
+from utils import load_sprite
+from utils import load_sprite, wrap_position, get_random_velocity
+
+UP = Vector2(0, -1)
 
 class GameObject:
     def __init__(self, position, sprite, velocity):
@@ -11,9 +16,38 @@ class GameObject:
         blit_position = self.position - Vector2(self.radius)
         surface.blit(self.sprite, blit_position)
 
-    def move(self):
-        self.position = self.position + self.velocity
+    def move(self, surface):
+        self.position = wrap_position(self.position + self.velocity, surface)
 
-    def collides_with(self, other_obg):
+    def collides_with(self, other_obj):
         distance = self.position.distance_to(other_obj.position)
         return distance < self.radius + other_obj.radius
+
+class Spaceship(GameObject):
+    MANEUVERABILITY = 3
+    ACCELERATION = 0.25
+
+    def __init__(self, position):
+        self.direction = Vector2(UP)
+        super().__init__(position, load_sprite("spaceship"), Vector2(0))
+    
+    def rotate(self, clockwise = True):
+        sign = 1 if clockwise else - 1
+        angle = self.MANEUVERABILITY * sign
+        self.direction.rotate_ip(angle)
+
+    def accelerate(self):
+        self.velocity += self.direction * self.ACCELERATION
+
+    def draw(self, surface):
+        angle = self.direction.angle_to(UP)
+        rotated_surface = rotozoom(self.sprite, angle, 1.0)
+        rotated_surface_size = Vector2(rotated_surface.get_size())
+        blit_position = self.position - rotated_surface_size * 0.5
+        surface.blit(rotated_surface, blit_position)
+
+class Asteroid(GameObject):
+    def __init__(self, position):
+        super().__init__(
+            position, load_sprite("asteroid"), get_random_velocity(1, 3)
+        )
